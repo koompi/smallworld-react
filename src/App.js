54ee97d1b6cfc75087015b5abb01883c9f49cb50
san-vuthy <san.vuthy08@gmail.com>
2019-11-29 11:19:38 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Route, Switch, Redirect } from "react-router-dom"
+import { BrowserRouter as Route, Switch, Redirect } from "react-router-dom"
 import Index from "./components/index"
 import AboutPage from "./components/about"
 import WhatWeCanDo from "./components/what-we-can-do"
@@ -8,9 +8,8 @@ import News from "./components/news-and-events"
 
 class App extends Component {
   render() {
-    window.scrollTo(0, 0)
     return (
-      <div className="App">
+      <Route>
         <Switch>
           <Route exact path="/" component={Index} />
           <Route exact path="/about-us" component={AboutPage} />
@@ -19,7 +18,7 @@ class App extends Component {
           <Route exact path="/contact-us" component={Contact} />
           <Redirect to="/" component={Index} />
         </Switch>
-      </div>
+      </Route>
     )
   }
 }
