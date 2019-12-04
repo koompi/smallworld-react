5c102a656a823e698ce406ba8ac87187ebf3b2bc
san-vuthy <san.vuthy08@gmail.com>
2019-12-04 17:04:53 +0700

    add server for contact page and add docker in app

---
+++ client/build/precache-manifest.c7d8dd359b620977472fdd03bea6b240.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b574215cd977ac4aaaaf1e00d61fcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df2e4d56a71c07dc5539",
+    "url": "/static/css/main.7cb0788c.chunk.css"
+  },
+  {
+    "revision": "01a427dfa4b577e054f4",
+    "url": "/static/js/2.f53c9eaf.chunk.js"
+  },
+  {
+    "revision": "df2e4d56a71c07dc5539",
+    "url": "/static/js/main.b35d4b74.chunk.js"
+  },
+  {
+    "revision": "f7917511fe8eb20cd49e",
+    "url": "/static/js/runtime-main.674419e7.js"
+  }
+]);
